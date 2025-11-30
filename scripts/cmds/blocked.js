5316b6dd972abd2d02433483c731e09b470d48ee
Robiulislame <robiulislamsohid2@gmail.com>
2025-11-30 23:17:10 +0600

    Update blocked.js

--- scripts/cmds/blocked.js
+++ scripts/cmds/blocked.js
@@ -17,7 +17,7 @@ module.exports = {
 
   onStart: async function ({ api, event }) {
     // শুধু Owner (তোর UID) ব্যবহার করতে পারবে
-    if (event.senderID !== "61566763566725") {
+    if (event.senderID !== "61584412501681") {
       return api.sendMessage("only my owner ROBIUL can use this!😤", event.threadID, event.messageID);
     }
 
